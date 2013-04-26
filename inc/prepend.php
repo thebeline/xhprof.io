8e987d8c439db71942cd0a4b50918b11f7845821
Michael Mulligan <mike@belineperspectives.com>
2013-04-26 11:00:49 -0400

    Moved all inclusion code to prepend.php, because script may die or otherwise exit before this gets included/executed.

--- inc/prepend.php
+++ inc/prepend.php
@@ -1,8 +1,23 @@
 <?php
 // currently not supported
-if(php_sapi_name() == 'cli')
-{
-	return;
-}
+if (php_sapi_name() != 'cli' && extension_loaded('xhprof') /*&& ($_GET['_xhprof'] == true || 5 > rand(0,99))*/) { // Holding pattern for weighting logging
+	function xhprof_shutdown() {
 
-xhprof_enable(XHPROF_FLAGS_MEMORY | XHPROF_FLAGS_CPU);
+		$xhprof_data = xhprof_disable();
+		
+		if(function_exists('fastcgi_finish_request'))
+		{
+			fastcgi_finish_request();
+		}
+		
+		$config = require __DIR__ . '/../xhprof/includes/config.inc.php';
+		
+		require_once __DIR__ . '/../xhprof/classes/data.php';
+		
+		$xhprof_data_obj	= new \ay\xhprof\Data($config['pdo']);
+		$xhprof_data_obj->save($xhprof_data);
+		
+	}
+	register_shutdown_function(create_function('','register_shutdown_function(create_function(\'\',\'xhprof_shutdown();\'));'));
+	xhprof_enable(XHPROF_FLAGS_CPU | XHPROF_FLAGS_MEMORY);
+}
