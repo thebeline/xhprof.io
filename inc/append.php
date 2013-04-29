76ea17ab82257beeda4111ea1320c236beac2d1a
Michael Mulligan <mike@belineperspectives.com>
2013-04-29 16:19:21 -0400

    Something like this?

--- inc/append.php
+++ inc/append.php
@@ -1,2 +1,3 @@
 <?php
-	error_log("Deprecated: xhprof.io - append.php - Please remove auto_append_file append.php, as this will break in the future. Address: ".$_SERVER['SERVER_NAME']."/".$_SERVER['REQUEST_URI']);
+	$where = (!empty($_SERVER)) ? ("Address: ".$_SERVER['SERVER_NAME']."/".$_SERVER['REQUEST_URI']) : "";
+	error_log("Deprecated: xhprof.io - append.php - Please remove auto_append_file append.php, as this will break in the future. ".$where);
