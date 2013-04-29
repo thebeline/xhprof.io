dcfd5c393b4fe7836f5c0b300e9b879ec2e767e5
Michael Mulligan <mike@belineperspectives.com>
2013-04-29 09:30:09 -0400

    Kept append.php and added deprecated error_log to aid in cleaning it up.

---
+++ inc/append.php
@@ -0,0 +1,2 @@
+<?php
+	error_log("Deprecated: xhprof.io - append.php - Please remove auto_append_file append.php, as this will break in the future. Address: ".$_SERVER['SERVER_NAME']."/".$_SERVER['REQUEST_URI']);
