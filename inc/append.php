d8e3699306bac9dc153c6857215b8b6e489c0e49
Michael Mulligan <mike@belineperspectives.com>
2013-04-29 17:59:15 -0400

    More info in the remove-me append.php file.  Christ, this better be enough.  :-D

--- inc/append.php
+++ inc/append.php
@@ -1,3 +1,3 @@
 <?php
-	$where = (!empty($_SERVER)) ? ("Address: ".$_SERVER['SERVER_NAME']."/".$_SERVER['REQUEST_URI']) : "";
-	error_log("Deprecated: xhprof.io - append.php - Please remove auto_append_file append.php, as this will break in the future. ".$where);
+	$where = (php_sapi_name() != 'cli') ? ("; A Virtual Host directive or .htaccess file associated with the address ".$_SERVER['SERVER_NAME']."/".$_SERVER['REQUEST_URI']) : "";
+	error_log("Deprecated: xhprof.io - append.php - Please remove auto_append_file append.php, as this will break in the future. Suggested places to look: Your php.ini; A manual inclusion as a result of executing [".$_SERVER['DOCUMENT_ROOT'].'/'.$_SERVER['PHP_SELF'].']'.$where);
