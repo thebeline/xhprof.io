ad45ee10b7ba998aac8bed35958213b22cdf9fc6
Markus Staab <markus.staab@redaxo.de>
2013-11-05 10:29:17 +0100

    moved all vars out of global scope.

--- inc/prepend.php
+++ inc/prepend.php
@@ -1,58 +1,67 @@
 <?php
 
-if (!extension_loaded('xhprof')) {
-	return;
+if (xhprof_init()) {
+    // The following registers an anonymous shutdown function that then registers another (end of stack)
+    // shutdown function that calls our actual function. Ensuring we run absolutely last.
+    register_shutdown_function(create_function('', 'register_shutdown_function("xhprof_shutdown");'));
 }
 
-// currently not supported on CLI
-if(php_sapi_name() == 'cli') {
-	return;
-}
 
-// do not profile debugging sessions (ZendDebugger)
-if (!empty($_COOKIE['start_debug'])) {
-    return;
-}
+function xhprof_init() {
+    global $xhprofMainConfig;
 
-// check for an app specific config which may override the global config:
-$appConfig = array();
-
-// Walk up the fielpath up to the DOCUMENT_ROOT to find the optional app config file
-// First file found on the path will be used
-$fileServed = $_SERVER['SCRIPT_FILENAME'];
-$dir = dirname($fileServed);
-do {
-    $cfgFile = $dir .DIRECTORY_SEPARATOR. 'xhprof.inc.php';
-    
-    if (file_exists($cfgFile)) {
-        $appConfig = require $cfgFile;
-        break;
+    if (!extension_loaded('xhprof')) {
+        return false;
     }
-    
-    $dir = dirname($dir);
-} while (!empty($dir) && stripos($dir, $_SERVER["DOCUMENT_ROOT"]) !== false);
 
-// disabled by app-config?
-if (isset($appConfig['enabled']) && !$appConfig['enabled']) {
-    return;
+    // currently not supported on CLI
+    if(php_sapi_name() == 'cli') {
+        return false;
+    }
+
+    // do not profile debugging sessions (ZendDebugger)
+    if (!empty($_COOKIE['start_debug'])) {
+        return false;
+    }
+
+    // check for an app specific config which may override the global config:
+    $appConfig = array();
+
+    // Walk up the fielpath up to the DOCUMENT_ROOT to find the optional app config file
+    // First file found on the path will be used
+    $fileServed = $_SERVER['SCRIPT_FILENAME'];
+    $dir = dirname($fileServed);
+    do {
+        $cfgFile = $dir .DIRECTORY_SEPARATOR. 'xhprof.inc.php';
+
+        if (file_exists($cfgFile)) {
+            $appConfig = require $cfgFile;
+            break;
+        }
+
+        $dir = dirname($dir);
+    } while (!empty($dir) && stripos($dir, $_SERVER["DOCUMENT_ROOT"]) !== false);
+
+    // disabled by app-config?
+    if (isset($appConfig['enabled']) && !$appConfig['enabled']) {
+        return;
+    }
+
+    $xhprofMainConfig = require __DIR__ . '/../xhprof/includes/config.inc.php';
+
+    // check the global enable switch, but only when no app-config enable switch was defined
+    if (!isset($appConfig['enabled']) && isset($xhprofMainConfig['profiler_enabled']) && !$xhprofMainConfig['profiler_enabled']) {
+        return false;
+    }
+
+    xhprof_enable(XHPROF_FLAGS_MEMORY | XHPROF_FLAGS_CPU);
+
+    return true;
 }
-
-$xhprofMainConfig = require __DIR__ . '/../xhprof/includes/config.inc.php';
-
-// check the global enable switch, but only when no app-config enable switch was defined
-if (!isset($appConfig['enabled']) && isset($xhprofMainConfig['profiler_enabled']) && !$xhprofMainConfig['profiler_enabled']) {
-    return;
-}
-
-xhprof_enable(XHPROF_FLAGS_MEMORY | XHPROF_FLAGS_CPU);
-
-// The following registers an anonymous shutdown function that then registers another (end of stack)
-// shutdown function that calls our actual function. Ensuring we run absolutely last.
-register_shutdown_function(create_function('', 'register_shutdown_function("xhprof_shutdown");'));
 
 function xhprof_shutdown() {
-	global $xhprofMainConfig;
-	
+    global $xhprofMainConfig;
+
 	$xhprof_data	= xhprof_disable();
 
 	if(function_exists('fastcgi_finish_request'))
